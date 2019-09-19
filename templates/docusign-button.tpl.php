b113ccc7da91237647efdaf7ef04d2ee93d538ce
delcesar <>
2019-09-19 11:18:50 -0400

    Drupal 7 Module: DocuSign Button for eSignatures

---
+++ templates/docusign-button.tpl.php
@@ -0,0 +1,7 @@
+<div id="docusign-button-container" class="mt-3" nid="<?php print $nid; ?>">
+    <a href="<?php print $url; ?>" class="btn btn-primary btn-lg">
+        <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> 
+        Sign documents
+    </a>
+</div>
+
